490be2a7c7606e3e354cd803624261859cb3cd82
AveryEclipse <thekayaurora@gmail.com>
2026-06-13 21:37:11 +0200

    yay pt2
    remove get-username and make linux whidbey executable

--- _plugins/get-username.rb
+++
@@ -1,9 +0,0 @@
-# this plugin is used to get the username of the PC from which the compilation process is initiated.
-# the username is displayed on the debug page
-module GetUsername
-  def username(input)
-    ENV['USER'] || ENV['USERNAME']
-  end
-end
-
-Liquid::Template.register_filter(GetUsername)
